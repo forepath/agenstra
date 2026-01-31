9fb90a9ddd18c2772c9d5a8c56dcec41c29cf70b
Marcel Menk <marcel.menk@forepath.io>
2026-01-31 14:53:03 +0100

    fix: ensure zip-bundled app functionality
    part of: no ticket

--- apps/native-agent-console/forge.config.js
+++ apps/native-agent-console/forge.config.js
@@ -13,10 +13,11 @@ module.exports = {
   makers: [
     ...(isWindows
       ? [
-          {
-            name: '@electron-forge/maker-zip',
+          /*{
+            name: '@electron-forge/maker-squirrel',
+            config: { name: 'agenstra' },
             platforms: ['win32'],
-          },
+          },*/
           {
             name: '@electron-forge/maker-zip',
             platforms: ['win32'],
