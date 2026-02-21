c81f18b6402749b5e37c608d14beccf2d08bfaf6
Marcel Menk <marcel.menk@forepath.io>
2026-02-21 23:26:29 +0100

    chore: remove faulty auto-generated frontend

--- libs/domains/framework/frontend/index.ts
+++ libs/domains/framework/frontend/index.ts
@@ -1,10 +1,8 @@
 // framework domain frontend exports
-export * from './feature-agent-console/src';
 export * from './data-access-agent-console/src';
-export * from './util-configuration/src';
-export * from './feature-portal/src';
+export * from './feature-agent-console/src';
 export * from './feature-docs/src';
-export * from './util-docs-parser/src';
+export * from './feature-portal/src';
+export * from './util-configuration/src';
 export * from './util-cookie-consent/src';
-export * from './data-access-billing-console/src';
-export * from './feature-billing-console/src';
+export * from './util-docs-parser/src';
