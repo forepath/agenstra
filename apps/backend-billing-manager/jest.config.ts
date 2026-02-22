8c27abaca9dc1fcfa2eada2071095ab4e7f55ea8
Marcel Menk <marcel.menk@forepath.io>
2026-02-22 13:05:15 +0100

    refactor: unified app and library names

--- apps/backend-billing-manager/jest.config.ts
+++ apps/backend-billing-manager/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: 'backend-billing',
+  displayName: 'backend-billing-manager',
   preset: '../../jest.preset.cjs',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/backend-billing',
+  coverageDirectory: '../../coverage/apps/backend-billing-manager',
 };
