16f96031d8b2ffbaed599bb5f24b1becc10bd1c3
Marcel <74356027+me-marcel@users.noreply.github.com>
2026-04-01 21:27:47 +0200

    feat: landingpage navbar shadow on scroll (#265)

--- libs/domains/framework/frontend/feature-portal/src/lib/container/container.component.ts
+++ libs/domains/framework/frontend/feature-portal/src/lib/container/container.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, inject, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LocaleService } from '@forepath/framework/frontend/util-configuration';
 
@@ -18,6 +18,19 @@ export class PortalContainerComponent {
    * Mobile menu visibility
    */
   readonly mobileMenuOpen = signal<boolean>(false);
+
+  /**
+   * True when the user has scrolled the page
+   */
+  readonly isScrolled = signal<boolean>(false);
+
+  /**
+   * Check if the user has scrolled the page
+   */
+  @HostListener('window:scroll')
+  onScroll(): void {
+    this.isScrolled.set(window.scrollY > 0);
+  }
 
   /**
    * Toggle mobile menu
